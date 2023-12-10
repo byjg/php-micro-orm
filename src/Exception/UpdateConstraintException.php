f6e58c6436d3eb4be5676daa1ec29288a4b6c986
Joao Gilberto Magalhaes <joao@byjg.com.br>
2023-12-09 21:37:54 -0600

    Add UpdateConstraint.php

---
+++ src/Exception/UpdateConstraintException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ByJG\MicroOrm\Exception;
+
+class UpdateConstraintException extends \Exception
+{
+
+}
--- src/UpdateConstraint.php
+++ src/UpdateConstraint.php
@@ -3,11 +3,12 @@
 namespace ByJG\MicroOrm;
 
 use ByJG\MicroOrm\Exception\AllowOnlyNewValuesConstraintException;
+use ByJG\MicroOrm\Exception\UpdateConstraintException;
 use ByJG\Serializer\SerializerObject;
 
 class UpdateConstraint
 {
-    private array $allowOnlyNewValuesForFields = [];
+    private array $closureValidation = [];
 
     public static function instance(): self
     {
@@ -16,23 +17,36 @@ class UpdateConstraint
 
     public function withAllowOnlyNewValuesForFields(array|string $properties): self
     {
-        $this->allowOnlyNewValuesForFields = array_merge($this->allowOnlyNewValuesForFields, (array)$properties);
+        $this->withClosureValidation(function($oldInstance, $newInstance) use ($properties) {
+            foreach ((array)$properties as $property) {
+                $method = "get" . ucfirst($property);
+                if (!method_exists($oldInstance, $method)) {
+                    throw new AllowOnlyNewValuesConstraintException("The property '$property' does not exists in the old instance.");
+                }
+                if (!method_exists($newInstance, $method)) {
+                    throw new AllowOnlyNewValuesConstraintException("The property '$property' does not exists in the new instance.");
+                }
+                if ($oldInstance->$method() == $newInstance->$method()) {
+                    throw new AllowOnlyNewValuesConstraintException("You are not updating the property '$property' ");
+                }
+            }
+            return true;
+        });
+        return $this;
+    }
+
+    public function withClosureValidation(\Closure $closure): self
+    {
+        $this->closureValidation[] = $closure;
         return $this;
     }
 
     public function check($oldInstance, $newInstance)
     {
-        foreach ($this->allowOnlyNewValuesForFields as $property) {
-            $method = "get" . ucfirst($property);
-            if (!method_exists($oldInstance, $method)) {
-                throw new AllowOnlyNewValuesConstraintException("The property '$property' does not exists in the old instance.");
-            }
-            if (!method_exists($newInstance, $method)) {
-                throw new AllowOnlyNewValuesConstraintException("The property '$property' does not exists in the new instance.");
-            }
-            if ($oldInstance->$method() == $newInstance->$method()) {
-                throw new AllowOnlyNewValuesConstraintException("You are not updating the property '$property' ");
-            }
+        foreach ($this->closureValidation as $closure) {
+            if ($closure($oldInstance, $newInstance) !== true) {
+                throw new UpdateConstraintException("The Update Constraint validation failed");
+            };
         }
     }
 }
