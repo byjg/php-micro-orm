e3ac59047a6f185e6e6d8b1f8139a7f50b91f9d9
HilarioJrx <hilariojrx@gmail.com>
2025-04-09 20:31:50 -0300

    fix: uuid generator

--- src/Attributes/TablePgsqlUuidPKAttribute.php
+++ src/Attributes/TablePgsqlUuidPKAttribute.php
@@ -4,7 +4,6 @@ namespace ByJG\MicroOrm\Attributes;
 
 use Attribute;
 use ByJG\AnyDataset\Db\DbDriverInterface;
-use ByJG\MicroOrm\Literal\Literal;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 class TablePgsqlUuidPKAttribute extends TableAttribute
@@ -12,7 +11,8 @@ class TablePgsqlUuidPKAttribute extends TableAttribute
     public function __construct(string $tableName)
     {
         parent::__construct($tableName, function (DbDriverInterface $dbDriver, object $entity) {
-            return bin2hex(random_bytes(16));
+            $bytes = random_bytes(16);
+            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($bytes), 4));
         });
     }
 }
